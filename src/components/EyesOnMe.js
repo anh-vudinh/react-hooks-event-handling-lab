a54527ad7fc8295d8caa689141f1c2b89f521291
Anh-vu <anhuvdinh@gmail.com>
2021-09-02 01:43:05 -0500

    commit tree 4553bc07d05246cb3e8bd59fd82f3a151a7c8abf

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -4,7 +4,7 @@ import React from 'react'
 function EyesOnMe(){
 
     return(
-        <button onFocus={()=>console.log("Good!")} onBlur={()=>console.log("Hey! Eyes on me!")}>Button</button>
+        <button onFocus={()=>console.log("Good!")} onBlur={()=>console.log("Hey! Eyes on me!")}>Eyes on me</button>
     )
 }
 
